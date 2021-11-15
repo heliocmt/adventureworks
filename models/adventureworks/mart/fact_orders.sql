0d6c857d3291a62df7c880c915d76af4d9aa4965
hcmtavora@gmail.com <hcmtavora@gmail.com>
2021-11-15 20:57:29 +0000

    Finalizado!

--- models/adventureworks/mart/fact_orders.sql
+++ models/adventureworks/mart/fact_orders.sql
@@ -58,38 +58,12 @@ with
         , creditcardid	
         from {{ ref('stg_orders') }}
     ),
-    
-    orders2 as (
-        select
-        orders.salesorderid
-        , salesreason_fk
-        , orders.customerid 
-        , orders.orderdate 
-        , orders.duedate 
-        , orders.shipdate 
-        , orders.subtotal 
-        , orders.taxamt 
-        , orders.freight 
-        , orders.totaldue
-        , orders.territoryid 
-        , orders.status
-        , orders.addressid 
-        , orders.shiptoaddressid 
-        , orders.shipmethodid 
-        , orders.creditcardid
-        , salesreasonid	
-        , reason	
-        , reasontype
-        from reasons as orders2
-        left join orders on orders2.salesorderid = orders.salesorderid
-    ),
         
     orders_with_sk as(     
-        select
+        select 
         salesorderid
         , customers.customer_fk
         , address1.address_fk
-        , salesreason_fk
         , creditcard.creditcard_fk
         , customers.customerid	
         , customers.personid	
@@ -109,12 +83,9 @@ with
         , address1.city	
         , address1.stateprovinceid
         , address1.postalcode	
-        , salesreasonid	
-        , reason	
-        , reasontype
         , creditcard.creditcardid
         , creditcard.cardtype
-        from orders2 as orders_with_sk
+        from orders as orders_with_sk
         left join address1 on orders_with_sk.addressid = address1.addressid
         left join creditcard on orders_with_sk.creditcardid = creditcard.creditcardid
         left join customers on orders_with_sk.customerid = customers.customerid
