60eabb89ae67e587cee344221c0f418af3733348
hcmtavora@gmail.com <hcmtavora@gmail.com>
2021-11-16 17:10:31 +0000

    dim_reasons ajustes

--- models/adventureworks/mart/dim_reasons.sql
+++ models/adventureworks/mart/dim_reasons.sql
@@ -6,12 +6,80 @@ with
     ),
     transformed as (
         select
-        row_number() over (order by salesreasonid) as salesreason_fk
+        row_number() over (order by salesorderid) as salesorder_sk
         , salesorderid		
         , salesreasonid
         , reason
         , reasontype
         from staging
-        order by salesreasonid
+    ),
+    orders as (
+        select 
+        row_number() over (order by salesorderid) as salesorder_fk
+        , customer_fk
+        , address_fk
+        , creditcard_fk
+        , customerid	
+        , firstname	
+        , lastname
+        , personid
+        , orderdate 
+        , duedate 
+        , shipdate 
+        , subtotal 
+        , taxamt 
+        , freight 
+        , totaldue
+        , status
+        , addressid 
+        , shiptoaddressid 
+        , shipmethodid 	
+        , addressline1			
+        , city	
+        , province
+        , country
+        , stateprovinceid
+        , postalcode	
+        , creditcardid
+        , cardtype
+        from  {{ ref('fact_orders') }}
+    ),
+    reasons as (
+        select
+        transformed.salesorderid
+        , salesorder_fk
+        , customer_fk
+        , address_fk
+        , creditcard_fk
+        , customerid	
+        , firstname	
+        , lastname
+        , personid
+        , orderdate 
+        , duedate 
+        , shipdate 
+        , subtotal 
+        , taxamt 
+        , freight 
+        , totaldue
+        , status
+        , addressid 
+        , shiptoaddressid 
+        , shipmethodid 	
+        , addressline1			
+        , city	
+        , province
+        , country
+        , stateprovinceid
+        , postalcode	
+        , creditcardid
+        , cardtype
+        , transformed.salesorder_sk	
+        , transformed.salesreasonid
+        , transformed.reason
+        , transformed.reasontype
+        from orders as reasons
+        left join transformed on reasons.salesorder_fk = transformed.salesorder_sk
     )
-    select * from transformed
+    select * from reasons
+    order by salesorderid
